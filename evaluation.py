53e333905cd8b7f3292763e6af6844c1204753ea
tkatyora <tkatyora7@gmail.com>
2023-08-09 14:17:11 +0200

    option2

--- evaluation.py
+++ evaluation.py
@@ -1,3 +1,4 @@
+import os
 print('Welcome To Telone Evaluation Calculator ')
 Rbz_rate = float(input('\nEnter Rbz Rate to use: '))
 quantity = int(input('Enter Quantity: '))
@@ -5,15 +6,29 @@ Vat_value = 1.15
 VatOption = input('''
 	      Select VAT option below 
 	       1. VAT INCLUDED : 
-	       2.VAT EXCLUDED :''')
+	       2.VAT EXCLUDED : 
+           3.Exit ''')
 
 while True:
-    
-	if VatOption == '1': 
 		UnitPrice = float(input('Supplier Unit Price: '))
-		UnitPriceVat = UnitPrice * Vat_value
-		TotalPrice = UnitPrice * quantity
-		UnitPriceUSD = UnitPriceVat / Rbz_rate
-		TotalPriceUSD = TotalPrice / Rbz_rate
-		print('Unit Price(ZWL) :  Unit Price(USD) : ')
+		if VatOption == '1':
+			TotalPrice = UnitPrice * quantity
+			UnitPriceUSD = UnitPrice / Rbz_rate
+			TotalPriceUSD = TotalPrice / Rbz_rate
+			print('Unit Price(ZWL) :  Unit Price(USD) : TotalPrice(zwl)')
+			print(UnitPriceVat  , UnitPriceUSD , TotalPrice )
+		elif VatOption == '2':
+			UnitPriceVat = UnitPrice * Vat_value
+			TotalPrice = UnitPriceVat * quantity
+			UnitPriceUSD = UnitPriceVat / Rbz_rate
+			TotalPriceUSD = TotalPrice / Rbz_rate
+			print('Unit Price(ZWL) :  Unit Price(USD) : TotalPrice(zwl)')
+			print(UnitPriceVat  , UnitPriceUSD , TotalPrice )
+		elif VatOption =='3':
+			os.sys.Exit()
+		else:
+			print(' Invalid Option Selected')
+      
+     
+         
   
