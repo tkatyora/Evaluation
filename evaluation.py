5d2848ffbb5a43ac57331394a0d0ae555805857a
tkatyora <tkatyora7@gmail.com>
2023-08-09 12:36:54 +0200

    option1

--- evaluation.py
+++ evaluation.py
@@ -1,12 +1,19 @@
 print('Welcome To Telone Evaluation Calculator ')
 Rbz_rate = float(input('\nEnter Rbz Rate to use: '))
+quantity = int(input('Enter Quantity: '))
 Vat_value = 1.15
 VatOption = input('''
 	      Select VAT option below 
 	       1. VAT INCLUDED : 
 	       2.VAT EXCLUDED :''')
 
-
-if VatOption == '1':
-    UnitPrice = float(input('Supplier Unit Price: '))
-    UnitPriceVat = UnitPrice * Vat_value
+while True:
+    
+	if VatOption == '1': 
+		UnitPrice = float(input('Supplier Unit Price: '))
+		UnitPriceVat = UnitPrice * Vat_value
+		TotalPrice = UnitPrice * quantity
+		UnitPriceUSD = UnitPriceVat / Rbz_rate
+		TotalPriceUSD = TotalPrice / Rbz_rate
+		print('Unit Price(ZWL) :  Unit Price(USD) : ')
+  
